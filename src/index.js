f573804a221800aa75bf3253d502a6752f0a444f
xoliio <xoliio@gmail.com>
2020-11-19 15:33:42 -0800

    With Milan

--- src/index.js
+++ src/index.js
@@ -12,4 +12,73 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  getToys()
+
+  const form = document.querySelector('.add-toy-form')
+  form.addEventListener('submit', newToy)
+  
 });
+
+function getToys() {
+  const source = "http://localhost:3000/toys"
+  fetch(source)
+    .then(function(response){
+      // console.log(response)
+      return response.json()
+    })
+    .then(function(jsonStuff){
+      const toyColl = document.querySelector('#toy-collection')
+      toyColl.innerHTML = allToys(jsonStuff)
+      addToyListener()
+    })
+  }
+
+function addToyListener(){
+  const toys = document.querySelectorAll('.card')
+  toys.forEach((toy) => {
+    toy.querySelector('.like-btn').addEventListener('click', likeToy)
+  })
+}
+  
+function singleToy(toy){
+  return (
+    `<div class="card">
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar"/>
+      <p class='likes'> ${toy.likes} </p>
+      <button id="${toy.id}" class="like-btn"> Like <3</button>
+    </div>`
+    
+)}
+
+function allToys(toyArr){
+  return toyArr.map(singleToy).join('')
+}
+
+function newToy(e){
+  const source = "http://localhost:3000/toys"
+  e.preventDefault()
+  const nameVal = document.querySelector('input[name=name]').value
+  const imgVal = document.querySelector('input[name=image').value
+  const shinyToy = {
+    name: nameVal, 
+    image: imgVal,
+    likes: 0
+  }
+  fetch(source, {method: 'POST', headers:{
+      "Content-Type": "application/json",
+      Accept: "application/json"
+      }, body: JSON.stringify(shinyToy)
+    })
+    .then(response => response.json())
+    .then((newObj) => {
+      document.querySelector('#toy-collection').innerHTML += singleToy(newObj)
+    })
+} 
+
+function likeToy(e) {
+  const like = document.querySelector('.likes')
+  
+  console.log(e)
+}
+
